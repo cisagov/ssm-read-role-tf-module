0a2984eb3c704fbdf6cc756b930ebdb9d22ae6b3
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-09-13 15:16:10 -0400

    Rename output variable and touch up description

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "role_arn" {
+output "arn" {
   value       = aws_iam_role.the_role.arn
-  description = "The ARN corresponding to the IAM role to be used for reading certificate data for ${var.hostname}"
+  description = "The ARN corresponding to the IAM role to be used for reading certificate data for the specified hostname"
 }
